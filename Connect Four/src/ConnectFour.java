2fcf2d9a4ce9aec41aa6a6fb8e869bcf02bc2bbe
ÖMER BOZABA <omerbozaba@gmail.com>
2020-01-25 00:39:27 +0300

    Add files via upload

---
+++ Connect Four/src/ConnectFour.java
@@ -0,0 +1,264 @@
+/*131044080
+OMER BOZABA
+CSE 241
+Object Oriented Programming
+Fall 2017
+Homework # 8
+ */
+
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class ConnectFour {
+    //inner Cell class
+    private class Cell{
+        //variables
+        private int row;
+        private int col;
+        private char hucre;
+        //Constructors
+        public Cell(){
+            hucre = '.';
+        }
+        public Cell(int x,int y,char c){
+            row = x;
+            col = y;
+            hucre = c;
+        }
+        //setterCell
+        public void set(char c){
+            hucre = c;
+        }
+        //Getters
+        public int getRow(){ return row; }
+        public int getCol(){ return col; }
+        public char getValue(){return hucre;}
+    }//end Cell Class
+
+    //variables
+    private Cell gameCells[][];
+    private int player;
+    private int size;
+    private int end;
+    private int oyunModu;
+    private int hamleSayisi;
+
+
+    ConnectFour(){
+        player = 1;
+        size = 4;
+        hamleSayisi=0;
+        oyunModu = 1;
+        end =0;
+
+        gameCells = new Cell[size][size];
+
+    }
+
+    ConnectFour(int n,int gameMod){
+       //init
+        player = 1;
+        size = n;
+        hamleSayisi=0;
+        oyunModu = gameMod;
+        end =0;
+
+        gameCells = new Cell[size][size];
+
+        for(int i=0; i<size; ++i)
+            for (int j=0 ; j<size ; ++j) {
+                gameCells[i][j]= new Cell(i,j,'.');
+            }
+    }
+    //setters
+    public void setOyunModu(int mod) 	{	oyunModu= mod;		}
+    public void setHamleSayisi(int hS)  {	hamleSayisi=hS; if(hamleSayisi>=size*size) setGameEnd(); }
+    public void setPlayer(int pl)		{	player=pl;			}
+    public void setGameEnd()			{	end=1; 				}
+    public void yaz(int i,int j,char harf){   gameCells[i][j].set(harf); }
+    //getters
+    public int currentWidth()    {	return size;	    }
+    public int currentHeight()   {	return size;	    }
+    public int getOyunModu()     {	return oyunModu;	}
+    public int getHamleSayisi()  {	return hamleSayisi;	}
+    public int getPlayer()       {	return player; 		}
+    public int isGameEnd()       {	return end;			}
+    public char isCell(int i,int j){return gameCells[i][j].getValue();}
+
+    //control functions
+    private int yatayKontrol(char harf,int test){
+        int i=0,j=0,find=0;
+
+        for(i=0;i<currentHeight(); ++i)
+            for(j=0;j+3<currentWidth(); ++j)
+                if( gameCells[i][j].getValue() == harf &&
+                        gameCells[i][j+1].getValue() == harf &&
+                        gameCells[i][j+2].getValue() == harf &&
+                        gameCells[i][j+3].getValue() == harf
+                        ){
+                    find = 1;
+                }
+        return find;
+    }
+
+    private int dikeyKontrol(char harf,int test){
+
+        int i=0,j=0,find=0;
+
+        for(j=0;j<currentWidth();++j)
+            for(i=0;i+3<currentHeight();++i)
+                if( gameCells[i][j].getValue() == harf &&
+                        gameCells[i+1][j].getValue() == harf &&
+                        gameCells[i+2][j].getValue() == harf &&
+                        gameCells[i+3][j].getValue() == harf
+                        ){
+                    find =1;
+                }
+        return find;
+    }
+    private int sagCaprazKontrol(char harf,int test){
+        int i=0,j=0,find=0;
+
+        for(i=0;i+3<currentHeight();++i)
+            for(j=0;j+3<currentWidth();++j)
+                if( gameCells[i][j].getValue() == harf &&
+                        gameCells[i+1][j+1].getValue() == harf &&
+                        gameCells[i+2][j+2].getValue() == harf &&
+                        gameCells[i+3][j+3].getValue() == harf
+                        ){
+                    find = 1;
+                }
+        return find;
+    }
+    private int solCaprazKontrol(char harf,int test){
+        int i=0,j=0,find=0;
+
+        for(i=currentHeight()-4;i>=0;--i)
+            for(j=currentWidth()-1;j-3>=0;--j) {
+                if (gameCells[i][j].getValue() == harf &&
+                        gameCells[i + 1][j - 1].getValue() == harf &&
+                        gameCells[i + 2][j - 2].getValue() == harf &&
+                        gameCells[i + 3][j - 3].getValue() == harf
+                        ) {
+                    find = 1;
+                }
+            }
+        return find;
+    }
+    //4 farkli yonden control eder boardu
+    public int controls(char harf,int test){
+
+        int kazanan=0 , sonuc;
+
+        sonuc = (yatayKontrol(harf,test)   +
+                dikeyKontrol(harf,test)    +
+                sagCaprazKontrol(harf,test)+
+                solCaprazKontrol(harf,test)
+        );
+        if(sonuc!=0){
+
+            if(harf == 'X')
+                kazanan=1;
+            else
+                kazanan=2;
+            if(test==0)
+                setGameEnd();
+        }
+        return kazanan;
+    }
+
+    private int isLegal(char coordinate){
+
+        int i=0,j=0;
+
+        j = coordinate - 'a';/*sutun hesaplama*/
+        /*sutun degeri hatali girilmis ise*/
+        if(j<0 || j >= currentWidth()){
+            return -1;
+        }
+        /*asagidan yukari dogru , o sutunde ilk bulunan bos yer return edilir*/
+        for(i=size -1 ; i>=0 ; --i)
+            if(gameCells[i][j].getValue() == '.')
+                return i;
+        return -1;/*sutun dolu ise -1 return edilir*/
+    }
+
+    private char oyunBiterMi(char harf){
+
+        int i=0,j=0,row,col,bitis=1;/*bitis 1  CPU , bitis 2  user icin kontrol*/
+
+        char hamle='a';
+
+        if (harf=='O')/*harf 'X' ise bitis=1 , harf 'O' ise bitis=2*/
+            bitis = 2;
+
+        for(i=0; i<currentWidth() ;++i){
+            row=isLegal(hamle);
+            if(row!=-1){
+                col=hamle - 'a';
+                gameCells[row][col].set(harf);
+                if(controls(harf,1)==bitis){
+                    gameCells[row][col].set('.');
+                    return hamle;
+                }
+                else
+                    gameCells[row][col].set('.');
+            }
+            ++hamle;
+        }
+        return '0';
+    }
+
+    public char play(){
+
+        int i=0,row,col;
+        char hamle='a',hamle2;
+        /*hamle2 ,  2 hamle sonrasini hesaplamak icin*/
+
+        /*CPU oyunu tek hamlede bitirebilir mi?*/
+        hamle = oyunBiterMi('O');
+        if(hamle != '0')
+            return hamle;
+
+        /*Oyuncu oyunu tek hamlede bitirebilir mi?*/
+        hamle = 'a';
+        hamle = oyunBiterMi('X');
+        if(hamle != '0')
+            return hamle;
+
+        /*2 hamle Ä°lerisi iÃ§in atak hesaplamasÄ±*/
+        hamle = 'a';
+        for(i=0 ; i<currentWidth(); ++i){
+            row=isLegal(hamle);
+            if(row!=-1){
+                col=hamle - 'a';
+                gameCells[row][col].set('O');/*Eger buraya hamle yaparsa*/
+                hamle2 = oyunBiterMi('O');/*burada oyun tek hamlelik mi olur?*/
+                gameCells[row][col].set('.'); /*Oyle ise hamle'yi return eder*/
+                if(hamle2 != '0')
+                    return hamle;
+            }
+            ++hamle;
+        }
+
+	/*2 hamle sonrasinda oyun kazanilabilecek durumda
+	  degil ise random bir hamle yapar*/
+        //srand(time(NULL));
+        System.out.println("yapay");
+        do{
+            Random rand = new Random();
+            col = rand.nextInt(size);
+            System.out.println("COL :" + col);
+            hamle = 'a';
+            for (i=0 ;i<col; ++i)
+                ++hamle;        /*karakter artirma*/
+            System.out.println(hamle);
+            if(isLegal(hamle)!= -1)
+                return hamle;
+        }while(true);/*return edene kadar hamle arar*/
+    }
+
+}
---
+++ Connect Four/src/main.java
@@ -0,0 +1,180 @@
+/*131044080
+OMER BOZABA
+CSE 241
+Object Oriented Programming
+Fall 2017
+Homework # 8
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+
+public class main {
+
+    public static  void  main(String [] argv){
+
+        String mod,str;
+        char c;
+        int n,oyunModu=1;
+
+        do{//oyun modu alma
+            while( ( mod = JOptionPane.showInputDialog("Oyun Modu:  'P' or 'C'")).length() != 1 );
+            c= mod.charAt(0);
+        }while(c!='C' && c!='P');
+
+        //size input alma
+        do{
+            str = JOptionPane.showInputDialog("Size Giriniz: ");
+            n = Integer.parseInt(str);
+        }while(n<4);
+        //oyun modunu integer veriye cevirme , CF objesi icin
+        if(c == 'C')
+            oyunModu=1;
+        else if(c == 'P')
+            oyunModu=2;
+
+        //obje yapiliyor
+        ConnectFour obj= new ConnectFour(n,oyunModu);
+
+        //JFrame olusturma , size ve gorunurluk vs
+        JFrame frame = new JFrame("Connect Four");
+        frame.setSize(57*n,32*(n+2));
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //Jpanel olusturma GUi adli . pdften alinti
+        JPanel GUI = new JPanel();
+        GUI.setLayout(null);
+        //baska panel olusturup onu Gui paneline ekliyoruz daha sonra, butonlarla birlikte
+        JPanel dugmeler = new JPanel();
+        dugmeler.setLayout(null);
+        dugmeler.setLocation(0,0);
+        dugmeler.setSize(n*57,(n+2)*32);
+
+
+        /*Buton ekleme kismi
+          https://www.youtube.com/watch?v=1i9WBVnTwHA
+          buradan alinti , orada 1 buton ekliyordu ben dinamik yaptim
+          (n) kadar buton
+         */
+        JButton jb[] = new JButton[n];
+        int mesafe = 0;
+        c='A';
+        for(int i=0; i < n ; i++){
+            jb[i] = new JButton();
+            String s=""+c;
+            jb[i].setText(s);
+            jb[i].setSize(55,20);
+            jb[i].setLocation(mesafe ,0);
+            jb[i].setHorizontalAlignment(0);
+            dugmeler.add(jb[i]);//dugmeler paneline ekliyoruz
+            mesafe += 55;
+            ++c;
+        }
+        //oyun ekraninda kutular icin label kullandim. 3 farkli gif , bos ,sari ve kirmizi
+        JLabel  labels[][] = new JLabel[n][n];
+        int yukseklik=20;
+        for (int i=0; i<n ; ++i){
+            mesafe = 0;
+            for(int j=0; j<n; ++j){
+                labels[i][j] = new JLabel();
+                labels[i][j].setIcon(new ImageIcon("bos.gif"));
+                labels[i][j].setLocation(mesafe,yukseklik);
+                labels[i][j].setSize(55,20);
+                labels[i][j].setVisible(true);
+                mesafe += 55;
+                dugmeler.add(labels[i][j]);
+            }
+            yukseklik += 20;
+        }
+
+        //oyunun durumunu en altta gosteren label.  Oyunun bitip bitmedigini soyluyor
+        JLabel sonuc = new JLabel("Player 1");
+        sonuc.setLocation( (n+1)*23,(n+1)*20);
+        sonuc.setSize(100,20);
+        dugmeler.add(sonuc);//labeli dugmelere
+        GUI.add(dugmeler);//dugmeler de gui'ye add yapildi
+        frame.setContentPane(GUI);
+        frame.setVisible(true);
+
+
+        //buton click durumlari
+        for (int i=0; i<n ; ++i){
+            jb[i].addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+
+                    int kazanan = 0;
+                    char hamle = e.getActionCommand().charAt(0);//click edilen butonun column olarak atanmasi A,B,C,D vs.
+                    int j = hamle - 'A';//col. belirleme
+                    for (int i = obj.currentHeight() - 1; i >= 0 ; i--) {//o sutunda bos yerlerden en alttakinin row'u bulunur
+                        if (obj.isCell(i,j) == '.') {//o koordinat bos mu ?
+                            obj.setHamleSayisi(obj.getHamleSayisi()+1);//hamle sayisi artirma
+                            if(obj.getPlayer()==1) {//player 1 hamle sirasi ise
+                                labels[i][j].setIcon(new ImageIcon("red.gif"));//label renk degisimi
+                                obj.setPlayer(0);//sira degisimi
+                                sonuc.setText("Player 2");
+                                obj.yaz(i,j,'X');
+                                kazanan = obj.controls('X',0);//kazanildi mi? kontrol
+                            }
+                            else {//oyun sirasi player 2 de ise
+                                obj.yaz(i,j,'O');
+                                labels[i][j].setIcon(new ImageIcon("yellow.gif"));
+                                obj.setPlayer(1);//sira degisimi
+                                sonuc.setText("Player 1");
+                                kazanan = obj.controls('O',0);
+                            }
+
+                            break;//donguden cikilir
+                        }
+
+                    }//end for
+                    if(kazanan == 1){//player 1 kazanma durumu
+                        JOptionPane.showMessageDialog(null,"Player 1 Kazandi" ,"Oyun Sonucu",JOptionPane.INFORMATION_MESSAGE);
+                        System.exit(0);
+                    }
+                    else if(kazanan == 2){//player 2 kazanma durumu
+                        JOptionPane.showMessageDialog(null,"Player 2 Kazandi" ,"Oyun Sonucu",JOptionPane.INFORMATION_MESSAGE);
+                        System.exit(0);
+                    }
+
+                    else {//player 1'in pesine Computer oynar
+                        if (obj.getOyunModu() == 1 && obj.getPlayer()==0){
+                            hamle = obj.play();//objenin yapay zeka fonksiyonu hamle hesaplar
+                            j= hamle-'a';
+                            for (int i = obj.currentHeight() - 1; i >= 0; i--) {
+                                if (obj.isCell(i, j) == '.') {
+                                    obj.yaz(i, j, 'O');
+                                    labels[i][j].setIcon(new ImageIcon("yellow.gif"));
+                                    obj.setPlayer(1);
+                                    sonuc.setText("Player 1");
+                                    break;
+                                }
+                            }
+                        obj.setHamleSayisi(obj.getHamleSayisi() + 1);// hamlesayisi++
+                        }
+                        kazanan = obj.controls('O',0);
+                        if(kazanan == 2) {//Yapay zeka kazanma durumu
+                            JOptionPane.showMessageDialog(null, "Computer Kazandi", "Oyun Sonucu", JOptionPane.INFORMATION_MESSAGE);
+                            System.exit(0);
+                        }
+                    }
+                    if(obj.isGameEnd()==1) {//oyunun bittiginde sonuc labeli ne yazacak?
+                        if(kazanan == 0) {
+                            sonuc.setText("Oyun BERABERE BITTI");
+                            JOptionPane.showMessageDialog(null, "Oyun BERABERE BITTI", "Oyun Sonucu", JOptionPane.INFORMATION_MESSAGE);
+                            System.exit(0);
+                        }
+                        else
+                            sonuc.setText("Oyun BITTI");
+                    }
+                }
+            });
+        }
+
+    }
+
+
+}
